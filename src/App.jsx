dcee0fc6d53a63ac96edf02e6fb4f7a0bd5c5c9f
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-02-25 12:53:43 +0100

    add :  list posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
+
+
+import { useState } from "react"
+
+
 function App() {
-  
+
+  const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState('');
+
+  const handleNewPost = (e) => {
+    setNewPost(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPosts(() => [...posts, newPost])
+    setNewPost('')
+  }
+
 
   return (
+
     <>
-      
-       
+      <div className="container">
+        <h2>Aggiungi posts</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={newPost}
+            onChange={handleNewPost}
+            placeholder="Inserisci un titolo..."
+          />
+          <button type="submit">Aggiungi</button>
+        </form>
+
+        <ul>
+          {posts.map((post, index) => (
+            <li key={index}>
+              {post}
+            </li>
+          ))}
+        </ul>
+
+      </div>
     </>
   )
 }
